9921c314c9b63435607537b02fe3cd50ffe6c4d2
diverseAdmin <admin@itsdiverse.com>
2019-12-20 11:04:00 -0500

    graphQL playground install

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Diverse`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@itsDiverse`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,26 @@
 import React from "react"
+import {Link, graphQL, useStaticQuery} from 'gatsby'
 
-export default () => 
-<div style={{ textAlign: `center`, height: `100px`, backgroundColor: `#eee` }}>
-  <h3>Header</h3>
-</div>
+
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+    
+    return (
+      <div style={{ textAlign: `center`, height: `100px`, backgroundColor: `#eee` }}>
+      <h3>{data.site.siteMetadata.title}</h3>
+      </div>
+    )
+    
+
+}
+
+export default Header
+
