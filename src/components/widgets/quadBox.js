d13d943fe67de5cdd6d27f775b8fcf650be35f85
diverseAdmin <admin@itsdiverse.com>
2019-12-26 12:45:17 -0500

    components template wotking

--- src/components/widgets/quadBox.js
+++ src/components/widgets/quadBox.js
@@ -1,26 +1,69 @@
 import React from "react"
 import style from "../../styles/quadBox.module.css"
-export default () =>
+import { checkPropTypes } from "prop-types"
+export default (props) =>
 <>
 <div className={style.contentContainer}>
     <div style={{textAlign: `center`}}>
-        <h2>Seamless integrations with finance software</h2>
-        <h3>Automatically import transactions to your expense management and accounting software with our out-of-the-box integrations.</h3>
+<h2>{props.heading}</h2>
+        <h3>{props.blurb}</h3>
     </div>
 
-    <div className={style.quadBoxContainer}>
+    <div className={style.quadBoxContainer} style={{gridTemplateRows: props.boxHeight}}>
         <div className={style.quadBox}>
-            <div className={style.quadBoxLeft}>
+            <div className={style.quadBoxLeft} style={{backgroundColor: props.box1Color}}>
 
             </div>
             
             <div className={style.quadBoxText}> 
-                <h1>Expensify</h1>
-                <p>An application for expense management, receipt scanning, and business travel.</p>
+                <h1>{props.box1title}</h1>
+                <p>{props.box1blurb}</p>
+            </div>
+        
+        </div>
+
+        <div className={style.quadBox}>
+            <div className={style.quadBoxLeft} style={{backgroundColor: props.box2Color}}>
+
+            </div>
+            
+            <div className={style.quadBoxText}> 
+                <h1>{props.box2title}</h1>
+                <p>{props.box2blurb}</p>
+            </div>
+        
+        </div>
+
+        <div className={style.quadBox}>
+            <div className={style.quadBoxLeft} style={{backgroundColor: props.box3Color}}>
+
+            </div>
+            
+            <div className={style.quadBoxText}> 
+                <h1>{props.box3title}</h1>
+                <p>{props.box3blurb}</p>
+            </div>
+        
+        </div>
+
+        <div className={style.quadBox}>
+            <div className={style.quadBoxLeft} style={{backgroundColor: props.box4Color}}>
+
+            </div>
+            
+            <div className={style.quadBoxText}> 
+                <h1>{props.box4title}</h1>
+                <p>{props.box4blurb}</p>
             </div>
         
         </div>
     </div>
+
+   
+
+    
+
+   
     
 </div>
 </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,22 @@ const IndexPage = () => {
   return (
     <div>
       <Layout>
-        <QuadBox/>
-        <QuadBox/>
+        <QuadBox 
+          boxHeight='200px 200px'
+          heading='the heading works bro!' 
+          blurb='and here is the lipsum!'
+          box1title='A box title!'
+          box1blurb='a'
+          box1Color='blue'
+          box2title='2a'
+          box2blurb='2'
+          box3title='title!'
+          box3blurb='a 3rd'
+          box4title='A 4th box title!'
+          box4blurb='a 4th box description will go here'
+        />
+  
+        
        
       </Layout>
     </div>
