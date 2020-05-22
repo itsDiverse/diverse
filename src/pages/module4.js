f8c3da076ff4a555c5661072ed813b461dab42a0
luchux <lucianoguasco@gmail.com>
2020-05-22 09:30:15 -0700

    adding page 4 to index

---
+++ src/pages/module4.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/layout"
+
+import Module4 from "../components/widgets/module4"
+
+const Page4 = () => {
+  return (
+    <>
+      <Layout>
+        <Module4 />
+      </Layout>
+    </>
+  )
+}
+
+export default Page4
