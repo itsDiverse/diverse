de52d6aa077e20f48421976f2de45dca4b429fd1
luchux <lucianoguasco@gmail.com>
2020-05-23 11:16:06 -0700

    adding business page

---
+++ src/components/business/layout.js
@@ -0,0 +1,46 @@
+import React from "react"
+import { useMediaQuery } from "react-responsive"
+import AnimatedNavbar from "../navbar"
+import { default as Footer } from "../footer"
+
+const MediaNav = () => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" })
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-device-width: 1224px)",
+  })
+  if (isDesktopOrLaptop) return <AnimatedNavbar duration={300} />
+  if (isTabletOrMobile) return <div>here mobile nav</div>
+
+  return null
+}
+
+const MediaFooter = () => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" })
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-device-width: 1224px)",
+  })
+
+  if (isDesktopOrLaptop) return <Footer></Footer>
+  if (isTabletOrMobile) return <Footer></Footer>
+
+  return null
+}
+
+export default ({ children }) => (
+  <div style={{ display: "flex", flexDirection: "column" }}>
+    <MediaNav />
+    <div
+      style={{
+        maxWidth: "1200px",
+        margin: "auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {children}
+    </div>
+    <MediaFooter />
+  </div>
+)
