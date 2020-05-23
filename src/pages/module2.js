dd8ac09641abe0b230bde68793a7b8a4856a7660
luchux <lucianoguasco@gmail.com>
2020-05-22 19:27:17 -0700

    module 2 code

--- src/pages/module2.js
+++ src/pages/module2.js
@@ -1,15 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Module2 from "../components/widgets/module2"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Module2 />
   </Layout>
 )
 
