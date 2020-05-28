e898b73e1e1ad36a12fcc71acb492b257bb1acbe
luchux <lucianoguasco@gmail.com>
2020-05-27 18:41:06 -0700

    last changes in mobile navbar

--- src/components/mobileNavbar/mobileNavbar.js
+++ src/components/mobileNavbar/mobileNavbar.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react"
-import { MenuIcon, MobileNavBar } from "./mobileNavbar.styles"
+import {
+  MenuIcon,
+  MobileNavBar,
+  StyledBrandRow,
+  StyledBrandName,
+} from "./MobileNavbar.styles"
+import { IconApp } from "../icons"
+import { default as Menu } from "./Menu"
 
-const MobileNavbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-
+const MobileNavbar = ({ menuOpened, onToggleMenu }) => {
   return (
-    <MobileNavBar>
-      navbarMobile
-      <MenuIcon>
-        {isMenuOpen ? (
-          <div onClick={() => setIsMenuOpen(false)}> X </div>
-        ) : (
-          <div onClick={() => setIsMenuOpen(true)}> = </div>
-        )}
-      </MenuIcon>
-    </MobileNavBar>
+    <>
+      <MobileNavBar>
+        <StyledBrandRow>
+          <IconApp size={50}></IconApp>
+          <StyledBrandName>diverse</StyledBrandName>
+        </StyledBrandRow>
+        <MenuIcon>
+          {menuOpened ? (
+            <div onClick={() => onToggleMenu(false)}> X </div>
+          ) : (
+            <div onClick={() => onToggleMenu(true)}> = </div>
+          )}
+        </MenuIcon>
+      </MobileNavBar>
+      {menuOpened && <Menu />}
+    </>
   )
 }
 
--- src/components/mobileNavbar/mobileNavbar.styles.js
+++ src/components/mobileNavbar/mobileNavbar.styles.js
@@ -3,11 +3,26 @@ import styled from "styled-components"
 export const MobileNavBar = styled.div`
   display: flex;
   flex: 1;
-  background: red;
   flex-direction: row;
+  box-shadow: 0 1px 4px 1px #eee;
+  padding: 10px 20px;
+  flex-grow: 0;
 `
 export const MenuIcon = styled.div`
   display: flex;
   flex: 1;
-  justify-content: "flex-end";
+  justify-content: flex-end;
+  align-items: center;
+`
+
+export const StyledBrandRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  align-items: center;
+  justify-content: flex-start;
+`
+export const StyledBrandName = styled.h2`
+  text-transform: uppercase;
+  margin-left: 10px;
 `
