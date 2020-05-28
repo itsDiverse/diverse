d907fa470698f21d1e279b983ceeecad9fd3e825
luchux <lucianoguasco@gmail.com>
2020-05-27 18:39:00 -0700

    adding icon app

---
+++ src/components/icons/IconApp.js
@@ -0,0 +1,56 @@
+import React from "react"
+
+export default ({ size }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 177.85 179.66"
+    width={size}
+    height={size}
+  >
+    <title>Asset 3</title>
+    <g id="Layer_2" data-name="Layer 2">
+      <g id="Layer_1-2" data-name="Layer 1">
+        <polygon
+          fill="#bde0f7"
+          points="88.86 154.02 44.41 128.32 44.38 179.66 88.86 154.02"
+        />
+        <polygon
+          fill="#204e86"
+          points="133.34 128.38 88.89 102.68 88.86 154.02 133.34 128.38"
+        />
+        <polygon
+          class="#337ac8"
+          points="44.41 128.32 88.86 154.02 88.89 102.68 44.41 128.32"
+        />
+        <polygon
+          fill="#204e86"
+          points="133.37 77.04 177.81 102.74 177.85 51.4 133.37 77.04"
+        />
+        <polygon
+          fill="#bde0f7"
+          points="88.92 51.34 133.37 77.04 133.4 25.7 88.92 51.34"
+        />
+        <polygon
+          class="#337ac8"
+          points="177.85 51.4 133.4 25.7 133.37 77.04 177.85 51.4"
+        />
+        <polygon
+          fill="#204e86"
+          points="44.48 25.64 88.92 51.34 88.96 0 44.48 25.64"
+        />
+        <polygon
+          fill="#bde0f7"
+          points="0 51.28 44.45 76.98 44.48 25.64 0 51.28"
+        />
+        <polygon
+          class="#337ac8"
+          points="88.92 51.34 44.48 25.64 44.45 76.98 88.92 51.34"
+        />
+        <polygon
+          fill="#bde0f7"
+          points="133.34 128.38 177.78 154.08 177.81 102.74 133.34 128.38"
+        />
+      </g>
+    </g>
+  </svg>
+)
