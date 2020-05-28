c6f7e4429df80c58d3e66359a0acdfb89ec9390f
luchux <lucianoguasco@gmail.com>
2020-05-27 18:39:21 -0700

    adding icon play

---
+++ src/components/icons/IconPlay.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+export default ({ size }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 14.938 14.938"
+  >
+    <g transform="translate(-247.727 -847.955)">
+      <g transform="translate(247.727 847.955)">
+        <path
+          d="M255.2,862.893a7.447,7.447,0,1,1,2.907-.587A7.423,7.423,0,0,1,255.2,862.893Zm0-13.881a6.412,6.412,0,1,0,6.412,6.412A6.419,6.419,0,0,0,255.2,849.012Z"
+          transform="translate(-247.727 -847.955)"
+          fill="#195bb6"
+        />
+      </g>
+      <path
+        d="M298.611,892.044V898.2a.227.227,0,0,0,.317.232l5.9-3.1a.272.272,0,0,0,0-.464l-5.9-3.057A.227.227,0,0,0,298.611,892.044Z"
+        transform="translate(-46.082 -39.697)"
+        fill="#195bb6"
+      />
+    </g>
+  </svg>
+)
