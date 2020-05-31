52b099352a464eca52ff389d66b8bf663c04a7f3
luchux <lucianoguasco@gmail.com>
2020-05-30 23:58:04 -0700

    Layout component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,54 @@
+import React from "react"
+import { useMediaQuery } from "react-responsive"
+
+import { default as MobileNavbar } from "../mobileNavbar/MobileNavbar"
+import AnimatedNavbar from "../navbar"
+
+import { StyledLayoutWrapper, StyledLayoutMain } from "./Layout.styles"
+import { Footer } from "../Footer"
+
+//Todo: 1st will use the two different components for menu in desktop and mobile
+//because business wants that. But, keeping an eye opened in possible
+//responsive with styled components media queries.
+const MediaNav = ({ menuOpened, onToggleMenu }) => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" })
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-device-width: 1224px)",
+  })
+  if (isDesktopOrLaptop) return <AnimatedNavbar duration={300} />
+  if (isTabletOrMobile)
+    return (
+      <MobileNavbar
+        menuOpened={menuOpened}
+        onToggleMenu={onToggleMenu}
+      ></MobileNavbar>
+    )
+
+  return null
+}
+
+const MediaFooter = () => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" })
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-device-width: 1224px)",
+  })
+
+  if (isDesktopOrLaptop) return <Footer></Footer>
+  if (isTabletOrMobile) return <Footer></Footer>
+
+  return null
+}
+
+const Layout = ({ children }) => {
+  const [menuOpened, setMenuOpened] = React.useState(false)
+
+  return (
+    <StyledLayoutWrapper>
+      <MediaNav menuOpened={menuOpened} onToggleMenu={setMenuOpened} />
+      <StyledLayoutMain>{!menuOpened && children}</StyledLayoutMain>
+      <Footer />
+    </StyledLayoutWrapper>
+  )
+}
+
+export default Layout
---
+++ src/components/Layout/Layout.styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const StyledLayoutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  height: 100vh;
+`
+
+export const StyledLayoutMain = styled.div`
+  max-width: "1200px";
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
