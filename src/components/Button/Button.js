fe6de29bf500d11656b3f8cf17f4023f3ab381c3
luchux <lucianoguasco@gmail.com>
2020-05-30 23:57:45 -0700

    button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+export const Button = styled.button`
+  background: ${({ primary }) => (primary ? "#195BB6;" : "#EBEDF0;")};
+  color: ${({ primary }) => (primary ? "#FFF;" : "#4B4B4B;")};
+  padding: 8px 25px;
+  border-radius: ${({ rounded }) => (rounded ? "20px" : "6px")};
+  display: flex;
+  align-items: center;
+  font-size: 13.5px;
+  border: none;
+`
+export const SquaredButton = styled.button`
+  background: ${({ primary }) => (primary ? "#195BB6;" : "#EBEDF0;")};
+  color: ${({ primary }) => (primary ? "#FFF;" : "#4B4B4B;")};
+  padding: 14px 25px;
+  border-radius: ${({ rounded }) => (rounded ? "20px" : "6px")};
+  display: flex;
+  align-items: center;
+  font-size: 13.5px;
+  width: 60px;
+  height: 45px;
+  border: none;
+`
