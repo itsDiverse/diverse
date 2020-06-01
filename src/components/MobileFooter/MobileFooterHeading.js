0d023d0ea1f882450abdab105fe12cc71dff644f
luchux <lucianoguasco@gmail.com>
2020-06-01 15:28:16 -0700

    mobile footer

--- src/components/MobileFooter/MobileFooterHeading.js
+++ src/components/MobileFooter/MobileFooterHeading.js
@@ -1,3 +1,23 @@
 import React from "react"
+import { Brand } from "../Brand"
 
-export const MobileFooterHeading = () => <></>
+import {
+  StyledMobileFooterHeading,
+  StyledParagraph,
+  StyledGrayRow,
+} from "./MobileFooterHeading.styles"
+
+import { VideoCTA } from "../VideoCTA"
+
+export const MobileFooterHeading = () => (
+  <StyledMobileFooterHeading>
+    <Brand size={40} />
+    <StyledParagraph>
+      We develop multi-channel sales processes to help businesses attract,
+      close, and retain customers.
+    </StyledParagraph>
+    <StyledGrayRow>
+      <VideoCTA button />
+    </StyledGrayRow>
+  </StyledMobileFooterHeading>
+)
---
+++ src/components/MobileFooter/MobileFooterHeading.styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const StyledMobileFooterHeading = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 0px;
+`
+export const StyledGrayRow = styled.div`
+  padding: 10px 20px;
+  margin: 20px -20px;
+  background: #fbfbfb;
+  font-size: 15px;
+  align-items: center;
+  justify-content: space-around;
+`
+
+export const StyledParagraph = styled.p`
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 22px;
+  color: #707070;
+`
