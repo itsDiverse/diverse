b3d73b7ce7037e585e56d1a5aaf655fd8c38d994
luchux <lucianoguasco@gmail.com>
2020-06-01 13:43:15 -0700

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,17 @@ export default props => (
   <Layout>
     <div>
       <h1>hi</h1>
-      <h2>this is the main page writter in side layout</h2>
+      <h2>this is the main</h2>
+    </div>
+
+    <div>
+      <h2>h2</h2>
+      <h3>This is some other content</h3>
+    </div>
+
+    <div>
+      <h2>another</h2>
+      <h3>This is some other content</h3>
     </div>
   </Layout>
 )
