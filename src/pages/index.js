9bb3ca7bf09530cb78c93c82ee49a2edafb78b95
luchux <lucianoguasco@gmail.com>
2020-06-03 15:18:02 -0700

    icon for face and insta

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,14 @@ import React from "react"
 import { Layout } from "../components"
 export default props => (
   <Layout>
-    <div>
-      <h1>hi</h1>
-      <h2>this is the main</h2>
-    </div>
-
-    <div>
-      <h2>h2</h2>
-      <h3>This is some other content</h3>
-    </div>
-
-    <div>
-      <h2>another</h2>
-      <h3>This is some other content</h3>
+    <div style={{ height: "100vh", backgroundColor: "#888", width: "100%" }}>
+      <h1>Thi is content </h1>
+      <h1>Thi is content </h1>
+      <h1>Thi is content </h1>
+      <h1>Thi is content </h1>v<h1>Thi is content </h1>
+      <h1>Thi is content </h1>
+      <h1>Thi is content </h1>
+      <h1>Thi is content </h1>
     </div>
   </Layout>
 )
