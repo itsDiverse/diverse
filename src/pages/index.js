d35d517cdaa87ca025581c978ecd71009b13ba62
luchux <lucianoguasco@gmail.com>
2020-06-05 09:58:08 -0700

    index page logic and styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,3 @@
 import React from "react"
 import { Layout } from "../components"
-export default props => (
-  <Layout>
-    <div
-      style={{
-        height: "100vh",
-        backgroundColor: "rgba(136, 136, 136, 0.12)",
-        width: "100%",
-      }}
-    ></div>
-  </Layout>
-)
+export default props => <Layout></Layout>
