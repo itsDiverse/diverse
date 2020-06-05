e8e3c4759627e6be72baceac2102692341ccd662
luchux <lucianoguasco@gmail.com>
2020-06-05 09:57:01 -0700

    dropdown fixes

--- src/components/DropDown/DropDown.styles.js
+++ src/components/DropDown/DropDown.styles.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 export const StyledDropDown = styled.div`
   display: flex;
   flex-direction: column;
+  user-select: none;
 `
 
 export const StyledIcon = styled.div`
@@ -12,11 +13,13 @@ export const StyledIcon = styled.div`
   margin-right: 10px;
   color: #707070;
   text-align: left;
+  user-select: none;
 `
 export const StyledTitles = styled.div`
   flex-direction: column;
   flex: 1;
   flex-grow: 15;
+  user-select: none;
 `
 
 export const StyledTitle = styled.p`
@@ -24,15 +27,18 @@ export const StyledTitle = styled.p`
   font-size: 16px;
   line-height: 19px;
   color: #707070;
+  user-select: none;
 `
 export const StyledSubtitle = styled.p`
   font-weight: normal;
   font-size: 11.5px;
   line-height: 113.1%;
   color: #707070;
+  user-select: none;
 `
 export const StyledDrodDownContainer = styled.div`
   display: ${({ show }) => (show ? "flex" : "none")};
+  user-select: none;
 `
 
 export const StyledRow = styled.div`
@@ -40,4 +46,5 @@ export const StyledRow = styled.div`
   flex-direction: row;
   align-items: center;
   cursor: pointer;
+  user-select: none;
 `
