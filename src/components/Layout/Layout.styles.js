cdafa4b8baf5a66c34c4ee502ddc8ee8d6483ecf
luchux <lucianoguasco@gmail.com>
2020-06-06 01:27:16 -0700

    changing layout for ios

--- src/components/Layout/Layout.styles.js
+++ src/components/Layout/Layout.styles.js
@@ -3,17 +3,12 @@ import styled from "styled-components"
 export const StyledLayoutWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
-  flex: 1;
 `
 
 export const StyledLayoutMain = styled.div`
-  
-  heigth: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: stretch;
 
   @media screen 
   and (min-device-width: 1200px) 
