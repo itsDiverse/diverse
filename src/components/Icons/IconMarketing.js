4e8e138502a9a3689a70517d01d972892ec7d646
luchux <lucianoguasco@gmail.com>
2020-06-06 18:03:16 -0700

    syntax typos

--- src/components/Icons/IconMarketing.js
+++ src/components/Icons/IconMarketing.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-export const IconLogin = ({ color, size }) => (
+export const IconMarketing = ({ color, size }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="27.536"
