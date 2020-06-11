86ac3c3f1f1c1c7fecb111a99c20b0ae1d0e2573
tdelgio <tomas.dg@me.com>
2020-06-11 19:18:45 -0300

    adding widgets pages

--- src/pages/my-page.js
+++ src/pages/my-page.js
@@ -1,5 +1,57 @@
 import React from "react"
-import { Badge } from "../components"
+import PropTypes from 'prop-types'
+import { default as AnimateModule } from "../components/widgets/AnimatedModule"
+import { default as contentHeader } from "../components/widgets/contentHeader"
+import { default as Module1 } from "../components/widgets/module1"
+import { default as Module2 } from "../components/widgets/module2"
+import { default as Module3 } from "../components/widgets/module3"
+import { default as Module4 } from "../components/widgets/module4"
+import { default as Module5 } from "../components/widgets/module5"
+import { default as Module6 } from "../components/widgets/module6"
+import { default as Module7 } from "../components/widgets/module7"
+import { default as Module8 } from "../components/widgets/module8"
+import { default as Module9 } from "../components/widgets/module9"
+import { default as Module10 } from "../components/widgets/module10"
+import { default as Module11 } from "../components/widgets/module11"
+import { default as Module12 } from "../components/widgets/module12"
+import { default as Module13 } from "../components/widgets/module13"
+import { default as Module14 } from "../components/widgets/module14"
+import { default as Module15 } from "../components/widgets/module15"
+import { default as Module16 } from "../components/widgets/module16"
+import { default as Module17 } from "../components/widgets/module17"
+import { default as Module18 } from "../components/widgets/module18"
+import { default as Module19 } from "../components/widgets/module19"
+import { default as Module20 } from "../components/widgets/module20"
+import { default as Module21 } from "../components/widgets/module21"
+import { default as Module22 } from "../components/widgets/module22"
+import { default as QuadBox } from "../components/widgets/quadBox"
 
 
-export const sayHi = () => <Badge color="white"> Perro </Badge> 
+const page = () => 
+<div>
+  <contentHeader/>
+  <Module1/>
+  <Module2/>
+  <Module3/>
+  <Module5/>
+  <Module6/>
+  <Module7/>
+  <Module8/>
+  <Module9/>
+  <Module10/>
+  <Module11/>
+  <Module12/>
+  <Module13/>
+  <Module14/>
+  <Module15/>
+  <Module16/>
+  <Module17/>
+  <Module18/>
+  <Module19/>
+  <Module20/>
+  <Module21/>
+  <Module22/>
+  <QuadBox/>
+</div>
+
+export default page
