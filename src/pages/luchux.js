593ae1c4f9dc8b600dbe1872c14d35b05533ce82
tdelgio <tomas.dg@me.com>
2020-06-12 20:12:07 -0300

    remove my-page

--- src/pages/luchux.js
+++ src/pages/luchux.js
@@ -1,9 +1,37 @@
 import React from "react"
+import { ContentHeader } from "../components/widgets/ContentHeader"
+import { QuadBox } from "../components/widgets/QuadBox"
 import { Module1 } from "../components/widgets/Module1"
+import { Module1a } from "../components/widgets/Module1a"
+import { Module2 } from "../components/widgets/Module2"
+import { Module3 } from "../components/widgets/Module3"
+import { Module4 } from "../components/widgets/Module4"
 import { Module5 } from "../components/widgets/Module5"
+import { Module6 } from "../components/widgets/Module6"
+import { Module7 } from "../components/widgets/Module7"
+import { Module8 } from "../components/widgets/Module8"
+import { Module9 } from "../components/widgets/Module9"
+import { Module10 } from "../components/widgets/Module10"
+import { Module11 } from "../components/widgets/Module11"
+import { Module11a } from "../components/widgets/Module11a"
+import { Module12 } from "../components/widgets/Module12"
+import { Module13 } from "../components/widgets/Module13"
+import { Module14 } from "../components/widgets/Module14"
+import { Module15 } from "../components/widgets/Module15"
+import { Module16 } from "../components/widgets/Module16"
+import { Module17 } from "../components/widgets/Module17"
+import { Module18 } from "../components/widgets/Module18"
+import { Module19 } from "../components/widgets/Module19"
+import { Module20 } from "../components/widgets/Module20"
+import { Module21 } from "../components/widgets/Module21"
+import { Module22 } from "../components/widgets/Module22"
 
 export default () => (
   <>
+    <h1>ContentHeader</h1>
+    <ContentHeader />
+    <hr></hr>
+    <h1>Module1</h1>
     <Module1
       image1="https://picsum.photos/200/300"
       image2="https://picsum.photos/200/300"
@@ -14,6 +42,19 @@ export default () => (
       image7="https://picsum.photos/200/300"
     ></Module1>
     <hr></hr>
+    <h1>Module1a</h1>
+    <Module1a />
+    <hr></hr>
+    <h1>Module2</h1>
+    <Module2 />
+    <hr></hr>
+    <h1>Module3</h1>
+    <Module3 />
+    <hr></hr>
+    <h1>Module4</h1>
+    <Module4 />
+    <hr></hr>
+    <h1>Module5</h1>
     <Module5
       image1="https://picsum.photos/200/300"
       image2="https://picsum.photos/200/300"
@@ -23,5 +64,62 @@ export default () => (
       image6="https://picsum.photos/200/300"
       image7="https://picsum.photos/200/300"
     ></Module5>
+    <hr></hr>
+    <h1>Module6</h1>
+    <Module6 />
+    <hr></hr>
+    <h1>Module7</h1>
+    <Module7 />
+    <hr></hr>
+    <h1>Module8</h1>
+    <Module8 />
+    <hr></hr>
+    <h1>Module9</h1>
+    <Module9 />
+    <hr></hr>
+    <h1>Module10</h1>
+    <Module10 />
+    <hr></hr>
+    <h1>Module11</h1>
+    <Module11 />
+    <hr></hr>
+    <h1>Module11a</h1>
+    <Module11a />
+    <hr></hr>
+    <h1>Module12</h1>
+    <Module12 />
+    <hr></hr>
+    <h1>Module13</h1>
+    <Module13 />
+    <hr></hr>
+    <h1>Module14</h1>
+    <Module14 />
+    <hr></hr>
+    <h1>Module15</h1>
+    <Module15 />
+    <hr></hr>
+    <h1>Module16</h1>
+    <Module16 />
+    <hr></hr>
+    <h1>Module17</h1>
+    <Module17 />
+    <hr></hr>
+    <h1>Module18</h1>
+    <Module18 />
+    <hr></hr>
+    <h1>Module19</h1>
+    <Module19 />
+    <hr></hr>
+    <h1>Module20</h1>
+    <Module20 />
+    <hr></hr>
+    <h1>Module21</h1>
+    <Module21 />
+    <hr></hr>
+    <h1>Module22</h1>
+    <Module22 />
+    <hr></hr>
+    <h1>QuadBox</h1>
+    <QuadBox />
   </>
 )
