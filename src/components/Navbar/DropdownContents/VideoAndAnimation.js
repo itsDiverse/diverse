92688265640f7ad4d151a44a69ce6d82a0dd1132
Luciano Guasco <luchux@Lucianos-MacBook-Pro.local>
2020-07-03 11:02:29 -0700

    a bunch of styles

--- src/components/Navbar/DropdownContents/VideoAndAnimation.js
+++ src/components/Navbar/DropdownContents/VideoAndAnimation.js
@@ -1,69 +1,16 @@
 import React from "react"
-import { IconDesktopVideo } from "../../Icons"
-import {
-  DropdownEl,
-  DropdownSection,
-  Heading,
-  ListSection,
-  Logo,
-  ListPrimary,
-  DropdownButton,
-  DropdownTitle,
-} from "./Components.js"
+
+import { footerData } from "../../footer/Footer.data"
+import { GenericDropdownContainer } from "./GenericDropdownContainer"
 
 export const VideoAndAnimationDropdown = () => {
+  const graphicsData = footerData[2]
   return (
-    <DropdownEl>
-      <DropdownSection>
-        <ListPrimary>
-          <li>
-            <IconDesktopVideo />
-          </li>
-          <li>
-            <DropdownTitle>Video & Animation</DropdownTitle>
-          </li>
-          <li>
-            Bring Your vision to life with the assistance of one of out artists.
-          </li>
-          <li>
-            <DropdownButton>All Video & Animation Services</DropdownButton>
-          </li>
-        </ListPrimary>
-        <ListSection>
-          <li>
-            <Heading>Animation</Heading>
-          </li>
-          <li>Whiteboard & Animation Explainers</li>
-          <li>Logo Animation</li>
-          <li>3D Product Animation</li>
-          <li>Character Animation</li>
-          <li>
-            <Heading>Live Action</Heading>
-          </li>
-          <li>Live Action Explainers</li>
-          <li>Spokespersons Videos</li>
-          <li>
-            <Heading>Social & Marketing</Heading>
-          </li>
-          <li>Short Video Ads</li>
-          <li>Slideshows & Proms Videos</li>
-          <li>Lyric & Music Videos</li>
-        </ListSection>
-        <ListSection>
-          <li>
-            <Heading>Photography</Heading>
-          </li>
-          <li>Product Photography</li>
-          <li>Local Photography</li>
-          <li>
-            <Heading>Editing & Post Production</Heading>
-          </li>
-          <li>Video Editing</li>
-          <li>Subtitles & Captions</li>
-          <li>Visual Effects</li>
-          <li>Intros & Outros</li>
-        </ListSection>
-      </DropdownSection>
-    </DropdownEl>
+    <GenericDropdownContainer
+      title={graphicsData.title}
+      subtitle={graphicsData.subtitle}
+      buttonText={graphicsData.buttonText}
+      items={graphicsData.items}
+    />
   )
 }
