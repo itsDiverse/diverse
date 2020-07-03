a71020a99362078368ac16b98345fec298b2c32d
Luciano Guasco <luchux@Lucianos-MacBook-Pro.local>
2020-07-03 11:01:42 -0700

    digital marketing

--- src/components/Navbar/DropdownContents/DigitalMarketing.js
+++ src/components/Navbar/DropdownContents/DigitalMarketing.js
@@ -1,139 +1,16 @@
 import React from "react"
 
-import { IconDesktopMarketing } from "../../Icons"
-
-import {
-  DropdownEl,
-  DropdownSection,
-  Heading,
-  ListSection,
-  Logo,
-  ListPrimary,
-  DropdownButton,
-  DropdownTitle,
-} from "./Components.js"
+import { footerData } from "../../footer/Footer.data"
+import { GenericDropdownContainer } from "./GenericDropdownContainer"
 
 export const DigitalMarketingDropdown = () => {
-  const condition = window.innerWidth <= 1500
-  return <>{condition ? <Compact /> : <Primary />}</>
-}
-
-const Primary = () => {
+  const marketingData = footerData[0]
   return (
-    <DropdownEl>
-      <DropdownSection>
-        <ListPrimary>
-          <li>
-            <IconDesktopMarketing />
-          </li>
-          <li>
-            <DropdownTitle>Digital Marketing</DropdownTitle>
-          </li>
-          <li>
-            Bring Your vision to life with the assistance of one of out artists.
-          </li>
-          <li>
-            <DropdownButton>All Digital Marketing Services</DropdownButton>
-          </li>
-        </ListPrimary>
-        <ListSection>
-          <li>
-            <Heading>Social & Content</Heading>
-          </li>
-          <li>Social Media Marketing</li>
-          <li>Influencer Marketing</li>
-          <li>Content Marketing</li>
-          <li>Email Marketing</li>
-          <li>Video Marketing</li>
-          <li>Music Promotion</li>
-        </ListSection>
-        <ListSection>
-          <li>
-            <Heading>Search</Heading>
-          </li>
-          <li>SEO</li>
-          <li>SEM</li>
-          <li>Local listings</li>
-          <li>
-            <Heading>Research & Stratagy</Heading>
-          </li>
-          <li>Marketing Stratagy</li>
-          <li>Domain Research</li>
-          <li>Web Analytics</li>
-        </ListSection>
-        <ListSection>
-          <li>
-            <Heading>Platform & Industries</Heading>
-          </li>
-          <li>Mobile Marketing & Advertising</li>
-          <li>E-Commerce Marketing</li>
-          <li>Crowdfunding</li>
-          <li>
-            <Heading>Misc.</Heading>
-          </li>
-          <li>Web Traffic</li>
-          <li>Surveys</li>
-        </ListSection>
-      </DropdownSection>
-    </DropdownEl>
-  )
-}
-
-const Compact = () => {
-  return (
-    <DropdownEl>
-      <DropdownSection>
-        <ListPrimary>
-          <li>
-            <IconDesktopMarketing />
-          </li>
-          <li>
-            <DropdownTitle>Digital Marketing</DropdownTitle>
-          </li>
-          <li>
-            Bring Your vision to life with the assistance of one of out artists.
-          </li>
-          <li>
-            <DropdownButton>All Digital Marketing Services</DropdownButton>
-          </li>
-        </ListPrimary>
-        <ListSection>
-          <li>
-            <Heading>Social & Content</Heading>
-          </li>
-          <li>Social Media Marketing</li>
-          <li>Influencer Marketing</li>
-          <li>Content Marketing</li>
-          <li>Email Marketing</li>
-          <li>Video Marketing</li>
-          <li>Music Promotion</li>
-          <li>
-            <Heading>Platform & Industries</Heading>
-          </li>
-          <li>Mobile Marketing & Advertising</li>
-          <li>E-Commerce Marketing</li>
-          <li>Crowdfunding</li>
-        </ListSection>
-        <ListSection>
-          <li>
-            <Heading>Search</Heading>
-          </li>
-          <li>SEO</li>
-          <li>SEM</li>
-          <li>Local listings</li>
-          <li>
-            <Heading>Research & Stratagy</Heading>
-          </li>
-          <li>Marketing Stratagy</li>
-          <li>Domain Research</li>
-          <li>Web Analytics</li>
-          <li>
-            <Heading>Misc.</Heading>
-          </li>
-          <li>Web Traffic</li>
-          <li>Surveys</li>
-        </ListSection>
-      </DropdownSection>
-    </DropdownEl>
+    <GenericDropdownContainer
+      title={marketingData.title}
+      subtitle={marketingData.subtitle}
+      buttonText={marketingData.buttonText}
+      items={marketingData.items}
+    />
   )
 }
