f3db6338f21ca4d1a48d047a2c4e319afc2efd9c
tdelgio <tomas.dg@me.com>
2020-07-08 20:15:28 -0300

    add module without global styles

--- src/pages/navbar1.js
+++ src/pages/navbar1.js
@@ -1,8 +1,18 @@
 import React from "react"
 import { Layout } from "../components"
+import { QuadBox } from "../components/widgets/"
+
+import { theme } from "../theme"
+import { data } from "../data"
+import { images } from "../images"
 
 export default props => (
   <Layout>
     <h1>Lorem Title</h1>
+    <QuadBox
+      images={images.quadBox}
+      data={data.quadBox}
+      theme={theme.quadBox}
+    />
   </Layout>
 )
