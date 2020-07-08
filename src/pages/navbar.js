5a03f4870814e58fb19406f62b0090bb90dcfe9d
tdelgio <tomas.dg@me.com>
2020-07-08 19:49:55 -0300

    page without modules

---
+++ src/pages/navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Layout } from "../components"
+
+export default props => (
+  <Layout>
+    <h1>Lorem Title</h1>
+    <h2>Lorem Title</h2>
+  </Layout>
+)
---
+++ src/pages/navbar1.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Layout } from "../components"
+
+export default props => (
+  <Layout>
+    <h1>Lorem Title</h1>
+  </Layout>
+)
