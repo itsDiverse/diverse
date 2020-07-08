627eb8ffbaf19f77caedd0a9ca3a5232d5c6da2b
tdelgio <tomas.dg@me.com>
2020-07-08 20:55:01 -0300

    import style to the title

--- src/pages/navbar1.js
+++ src/pages/navbar1.js
@@ -1,18 +1,9 @@
 import React from "react"
 import { Layout } from "../components"
-import { QuadBox } from "../components/widgets/"
-
-import { theme } from "../theme"
-import { data } from "../data"
-import { images } from "../images"
+import "../styles/navbarFix.css"
 
 export default props => (
   <Layout>
-    <h1>Lorem Title</h1>
-    <QuadBox
-      images={images.quadBox}
-      data={data.quadBox}
-      theme={theme.quadBox}
-    />
+    <h1 className="title">Lorem Title</h1>
   </Layout>
 )
