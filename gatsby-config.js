4a7312a548ebae71cba3ee8d5a3787730565e95b
Luciano Guasco <luchux@Lucianos-MacBook-Pro.local>
2020-07-11 11:57:37 -0700

    added robots config and package for it

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,12 @@ module.exports = {
     "gatsby-plugin-eslint",
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-css-modules`,
-
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        policy: [{ userAgent: "*", disallow: "/" }],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
