dab38f7b301149bf76e508e378cee2043227eac7
tdelgio <tomas.dg@me.com>
2020-08-31 15:35:18 -0300

    change description site

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `Diverse`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Marketing`,
     author: `@itsDiverse`,
   },
   plugins: [
