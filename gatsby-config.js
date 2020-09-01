f512cf26c73392e936db4708709af4c77f4d260b
tdelgio <tomas.dg@me.com>
2020-09-01 17:02:55 -0300

    manifiest name site

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,21 +30,19 @@ module.exports = {
         logo: "./static/favicon.png",
       },
     },
-
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp` /*
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Diverse`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },*/,
+    },
     "gatsby-plugin-eslint",
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-css-modules`,
