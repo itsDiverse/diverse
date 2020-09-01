8ca1397821b565eb5260ec5adbf8727548bb5deb
tdelgio <tomas.dg@me.com>
2020-09-01 17:10:00 -0300

    change name modalHtml

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,19 +30,21 @@ module.exports = {
         logo: "./static/favicon.png",
       },
     },
+
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sharp` /*
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Diverse`,
+        name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
+    },*/,
     "gatsby-plugin-eslint",
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-css-modules`,
