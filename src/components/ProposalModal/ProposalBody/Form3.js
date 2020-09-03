3debe4dee5eddcc1d3ef9bda9421a4b59327808a
tdelgio <tomas.dg@me.com>
2020-09-03 16:14:36 -0300

    rm wrapper

--- src/components/ProposalModal/ProposalBody/Form3.js
+++ src/components/ProposalModal/ProposalBody/Form3.js
@@ -6,7 +6,7 @@ import {
   StyledProposalInput,
   ErrorValidation,
 } from "../Proposal.styles"
-import { Formik, ErrorMessage, Form, Input } from "formik"
+import { Formik, ErrorMessage, Form } from "formik"
 import * as Yup from "yup"
 
 const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
@@ -52,70 +52,68 @@ export const Form3 = ({ setStep, formData, setFormData }) => (
     }}
   >
     {({ errors, touched, handleChange, handleBlur, onSubmit }) => (
-      <StyledProposalForm>
-        <Form
-          name="contact"
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          style={{ textAlign: "center" }}
-        >
-          <Input type="hidden" name="form-name" value="contact" />
-          <StyledProposalLabel>
-            What’s your website?
-            <StyledProposalInput
-              type="string"
-              name="website"
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-          </StyledProposalLabel>
-          <ErrorValidation>
-            <ErrorMessage name="website" />
-          </ErrorValidation>
-          <StyledProposalLabel>
-            What’s your name?
-            <StyledProposalInput
-              type="text"
-              name="name"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              error={touched.name && errors.name}
-            />
-          </StyledProposalLabel>
-          <ErrorValidation>
-            <ErrorMessage name="name" />
-          </ErrorValidation>
-          <StyledProposalLabel>
-            What’s your email?
-            <StyledProposalInput
-              type="email"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              error={touched.email && errors.email}
-            />
-          </StyledProposalLabel>
-          <ErrorValidation>
-            <ErrorMessage name="email" />
-          </ErrorValidation>
-          <StyledProposalLabel>
-            What’s your number?
-            <StyledProposalInput
-              type="string"
-              name="number"
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-          </StyledProposalLabel>
-          <ErrorValidation>
-            <ErrorMessage name="number" />
-          </ErrorValidation>
-          <StyledProposalButton type="submit">
-            SEND MY FREE PROPOSAL
-          </StyledProposalButton>
-        </Form>
-      </StyledProposalForm>
+      <Form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        style={{ textAlign: "center" }}
+      >
+        <StyledProposalInput type="hidden" name="form-name" value="contact" />
+        <StyledProposalLabel>
+          What’s your website?
+          <StyledProposalInput
+            type="string"
+            name="website"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </StyledProposalLabel>
+        <ErrorValidation>
+          <ErrorMessage name="website" />
+        </ErrorValidation>
+        <StyledProposalLabel>
+          What’s your name?
+          <StyledProposalInput
+            type="text"
+            name="name"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.name && errors.name}
+          />
+        </StyledProposalLabel>
+        <ErrorValidation>
+          <ErrorMessage name="name" />
+        </ErrorValidation>
+        <StyledProposalLabel>
+          What’s your email?
+          <StyledProposalInput
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.email && errors.email}
+          />
+        </StyledProposalLabel>
+        <ErrorValidation>
+          <ErrorMessage name="email" />
+        </ErrorValidation>
+        <StyledProposalLabel>
+          What’s your number?
+          <StyledProposalInput
+            type="string"
+            name="number"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </StyledProposalLabel>
+        <ErrorValidation>
+          <ErrorMessage name="number" />
+        </ErrorValidation>
+        <StyledProposalButton type="submit">
+          SEND MY FREE PROPOSAL
+        </StyledProposalButton>
+      </Form>
     )}
   </Formik>
 )
