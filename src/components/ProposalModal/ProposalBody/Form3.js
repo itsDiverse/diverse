27885652357ba4dc7f75446fbe37ed23fabe6139
tdelgio <tomas.dg@me.com>
2020-09-03 20:34:20 -0300

    add functions dir

--- src/components/ProposalModal/ProposalBody/Form3.js
+++ src/components/ProposalModal/ProposalBody/Form3.js
@@ -38,7 +38,7 @@ export const Form3 = ({ setStep, formData, setFormData }) => (
     }}
     validationSchema={FormSchema}
     onSubmit={values => {
-      fetch("https://www.meetdiverse.com/freeProposal", {
+      fetch("/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: encode({
