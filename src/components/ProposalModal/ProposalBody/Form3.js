dc43f03e8b52976a522d961565d3e08367fa898f
tdelgio <tomas.dg@me.com>
2020-09-03 20:08:43 -0300

    instal netlify plugin

--- src/components/ProposalModal/ProposalBody/Form3.js
+++ src/components/ProposalModal/ProposalBody/Form3.js
@@ -38,7 +38,7 @@ export const Form3 = ({ setStep, formData, setFormData }) => (
     }}
     validationSchema={FormSchema}
     onSubmit={values => {
-      fetch("https://www.meetdiverse.com/page-data/app-data.json", {
+      fetch("/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: encode({
