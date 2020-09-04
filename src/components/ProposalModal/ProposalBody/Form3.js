e6532c7fe21853cbbe21302b9c6f159e1813b00f
tdelgio <tomas.dg@me.com>
2020-09-03 21:26:34 -0300

    change name

--- src/components/ProposalModal/ProposalBody/Form3.js
+++ src/components/ProposalModal/ProposalBody/Form3.js
@@ -16,7 +16,7 @@ export const Form3 = ({ setStep, formData, setFormData }) => (
     method="POST"
     data-netlify-honeypot="bot-field"
     data-netlify="true"
-    name="contact"
+    name="Contact Form 1"
     style={{ textAlign: "center" }}
   >
     <input type="hidden" name="bot-field" />
