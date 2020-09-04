eaee5e3948ec19f8951e1f9ff119ecfa696b8a65
tdelgio <tomas.dg@me.com>
2020-09-03 21:55:17 -0300

    add state.jason

--- src/components/ProposalModal/ProposalBody/Form3.js
+++ src/components/ProposalModal/ProposalBody/Form3.js
@@ -1,9 +1,9 @@
 import React from "react"
 import {
   StyledProposalForm,
-  label,
+  StyledProposalLabel,
   StyledProposalButton,
-  input,
+  StyledProposalInput,
   ErrorValidation,
 } from "../Proposal.styles"
 import { Formik, ErrorMessage, Form } from "formik"
@@ -11,31 +11,111 @@ import * as Yup from "yup"
 
 const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
+const FormSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "*Too Short!")
+    .max(70, "*Too Long!")
+    .required(""),
+  email: Yup.string()
+    .email("*Invalid email")
+    .required("*Required"),
+  number: Yup.string().matches(phoneRegExp, "Phone number is not valid"),
+})
+
 export const Form3 = ({ setStep, formData, setFormData }) => (
-  <form
-    name="contact"
-    method="post"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field"
+  <Formik
+    initialValues={{
+      website: "",
+      name: "",
+      email: "",
+      number: "",
+    }}
+    validationSchema={FormSchema}
+    onSubmit={values => {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({
+          "form-name": "contact",
+          ...values,
+          ...formData,
+        }),
+      }).then(() => {
+        console.log(values, formData)
+        setStep(4)
+      })
+    }}
   >
-    <input type="hidden" name="form-name" value="contact" />
-    <p>
-      <label>
-        Your Name: <input type="text" name="name" />
-      </label>
-    </p>
-    <p>
-      <label>
-        Your Email: <input type="email" name="email" />
-      </label>
-    </p>
-    <p>
-      <label>
-        Message: <textarea name="message"></textarea>
-      </label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
+    {({ errors, touched, handleChange, handleBlur, onSubmit }) => (
+      <Form
+        method="post"
+        data-netlify-honeypot="bot-field"
+        data-netlify="true"
+        name="contact"
+        style={{ textAlign: "center" }}
+      >
+        <StyledProposalInput type="hidden" name="bot-field" />
+        <StyledProposalInput type="hidden" name="form-name" value="contact" />
+        <StyledProposalLabel>
+          What’s your website?
+          <StyledProposalInput
+            type="string"
+            name="website"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </StyledProposalLabel>
+        <ErrorValidation>
+          <ErrorMessage name="website" />
+        </ErrorValidation>
+        <StyledProposalLabel>
+          What’s your name?
+          <StyledProposalInput
+            type="text"
+            name="name"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.name && errors.name}
+          />
+        </StyledProposalLabel>
+        <ErrorValidation>
+          <ErrorMessage name="name" />
+        </ErrorValidation>
+        <StyledProposalLabel>
+          What’s your email?
+          <StyledProposalInput
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.email && errors.email}
+          />
+        </StyledProposalLabel>
+        <ErrorValidation>
+          <ErrorMessage name="email" />
+        </ErrorValidation>
+        <StyledProposalLabel>
+          What’s your number?
+          <StyledProposalInput
+            type="string"
+            name="number"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </StyledProposalLabel>
+        <ErrorValidation>
+          <ErrorMessage name="number" />
+        </ErrorValidation>
+        <StyledProposalButton type="submit">
+          SEND MY FREE PROPOSAL
+        </StyledProposalButton>
+      </Form>
+    )}
+  </Formik>
 )
