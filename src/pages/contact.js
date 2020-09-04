30a3a83bf62959e46ec4d1123ffa1004b359ab67
tdelgio <tomas.dg@me.com>
2020-09-04 12:59:58 -0300

    add input hidden fields

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,6 +9,8 @@ export default () => (
       data-netlify="true"
       data-netlify-honeypot="bot-field"
     >
+      <input type="hidden" name="bot-field" />
+      <input type="hidden" name="form-name" value="contact" />
       <p>
         <label>
           Your Name: <input type="text" name="goals" />
