1e12723acc8ae221858ba859b6d0b333f466cd75
tdelgio <tomas.dg@me.com>
2020-09-05 17:13:36 -0300

    add image to mobilenavbar

--- src/components/Navbar/logoMobile.js
+++ src/components/Navbar/logoMobile.js
@@ -1,81 +1,10 @@
 import React from "react"
 
 export const LogoMobile = () => (
-  <svg
-    width="46"
+  <img
+    src={require("../../images/logoDiverse.png")}
+    alt="logo"
     height="45"
-    viewBox="0 0 56 55"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g filter="url(#filter0_i)">
-      <path
-        d="M27.85 47.0726L13.92 39.2141V54.9018L27.85 47.0726Z"
-        fill="#BDE0F7"
-      />
-      <path
-        d="M41.7899 39.2337L27.86 31.385V47.0727L41.7899 39.2337Z"
-        fill="#204E86"
-      />
-      <path
-        d="M13.92 39.2144L27.85 47.0729V31.3755L13.92 39.2144Z"
-        fill="#337AC8"
-      />
-      <path
-        d="M41.8 23.5462L55.73 31.3949V15.7073L41.8 23.5462Z"
-        fill="#204E86"
-      />
-      <path
-        d="M27.87 15.6876L41.8 23.5461V7.8584L27.87 15.6876Z"
-        fill="#BDE0F7"
-      />
-      <path
-        d="M55.75 15.7073L41.82 7.84888V23.5463L55.75 15.7073Z"
-        fill="#337AC8"
-      />
-      <path d="M13.94 7.79996L27.87 15.6487V0L13.94 7.79996Z" fill="#204E86" />
-      <path d="M0 15.6683L13.93 23.5267V7.80005L0 15.6683Z" fill="#BDE0F7" />
-      <path
-        d="M27.87 15.6878L13.94 7.80005V23.5267L27.87 15.6878Z"
-        fill="#337AC8"
-      />
-      <path
-        d="M41.79 39.2337L55.72 47.0922V31.3948L41.79 39.2337Z"
-        fill="#BDE0F7"
-      />
-    </g>
-    <defs>
-      <filter
-        id="filter0_"
-        x="0"
-        y="0"
-        width="55.75"
-        height="57.9018"
-        filterUnits="userSpaceOnUse"
-        colorInterpolationFilters="sRGB"
-      >
-        <feFlood floodOpacity="0" result="BackgroundImageFix" />
-        <feBlend
-          mode="normal"
-          in="SourceGraphic"
-          in2="BackgroundImageFix"
-          result="shape"
-        />
-        <feColorMatrix
-          in="SourceAlpha"
-          type="matrix"
-          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-          result="hardAlpha"
-        />
-        <feOffset dy="3" />
-        <feGaussianBlur stdDeviation="2" />
-        <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
-        <feColorMatrix
-          type="matrix"
-          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"
-        />
-        <feBlend mode="normal" in2="shape" result="effect1_innerShadow" />
-      </filter>
-    </defs>
-  </svg>
+    width="45"
+  />
 )
