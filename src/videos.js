fc443256f3bd7577a8ee00a13336e478975d9e99
tdelgio <tomas.dg@me.com>
2020-09-23 14:45:21 -0300

    change src video with url

--- src/videos.js
+++ src/videos.js
@@ -3,7 +3,7 @@ import sample from "./videos/sample.mp4"
 import sample2 from "./videos/A_Web_Programming_1.mp4"
 import sampleVideo from "./videos/samplevideo.mp4"
 import playIcon from "./images/Module2/play.svg"
-import cubePipesVideo from "../static/cubePipes.mp4"
+//import cubePipesVideo from "../static/cubePipes.mp4"
 import cubesVideo from "../static/cubes_2.mp4"
 import cubesObjects from "../static/cubesObjects.mp4"
 import comingSoon from "../static/comingSoon.mp4"
@@ -12,7 +12,8 @@ import comingSoon from "../static/comingSoon.mp4"
 export const videos = {
   //---------------------------------Module 0
   module0: {
-    cubePipesVideo,
+    cubePipesVideo:
+      "https://5f66c0be9dfb670007ef6c52–itsdiverse.netlify.app/cubepipes.mp4",
   },
   //---------------------------------Module 2
   module2: {
