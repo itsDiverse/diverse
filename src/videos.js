b8967eb78b38fd77acb419b3001310a54dcaf821
tdelgio <tomas.dg@me.com>
2020-09-28 13:12:54 -0300

    replace sample videos

--- src/videos.js
+++ src/videos.js
@@ -13,8 +13,8 @@ export const videos = {
   //---------------------------------Module 0
   module0: {
     cubePipesVideo: "https://www.meetdiverse.com/cubepipes.mp4",
-    cubePipesVideo1: "https://www.meetdiverse.com/sample.m4v",
-    cubePipesVideo2: "https://www.meetdiverse.com/samplewebm.webm",
+    cubePipesVideo1: "https://www.meetdiverse.com/cubepipes.m4v",
+    cubePipesVideo2: "https://www.meetdiverse.com/cubepipes.webm",
   },
   //---------------------------------Module 2
   module2: {
@@ -39,14 +39,14 @@ export const videos = {
   //---------------------------------Module 23
   module23: {
     cubesVideo: "https://www.meetdiverse.com/cubes_2.mp4",
-    cubesVideo1: "https://www.meetdiverse.com/sample.m4v",
-    cubesVideo2: "https://www.meetdiverse.com/samplewebm.webm",
+    cubesVideo1: "https://www.meetdiverse.com/cubes_2.m4v",
+    cubesVideo2: "https://www.meetdiverse.com/cubes_2.webm",
   },
   //---------------------------------Module 24
   module24: {
     cubesObjects: "https://www.meetdiverse.com/cubesobjects.mp4",
-    cubesObjects1: "https://www.meetdiverse.com/sample.m4v",
-    cubesObjects2: "https://www.meetdiverse.com/samplewebm.webm",
+    cubesObjects1: "https://www.meetdiverse.com/cubesobjects.m4v",
+    cubesObjects2: "https://www.meetdiverse.com/cubesobjects.webm",
   },
   //---------------------------------ComingSoon Page
   comingSoon: {
