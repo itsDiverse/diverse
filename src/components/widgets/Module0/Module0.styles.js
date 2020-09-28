bd775b5569374c9ee8f51d407a75a32629831827
tdelgio <tomas.dg@me.com>
2020-09-28 11:13:24 -0300

    remove play button on overlay

--- src/components/widgets/Module0/Module0.styles.js
+++ src/components/widgets/Module0/Module0.styles.js
@@ -75,6 +75,9 @@ export const StyledContentWrapper = styled.div`
       -webkit-mask-image: -webkit-radial-gradient(white, black);
       -webkit-backface-visibility: hidden;
       -moz-backface-visibility: hidden;
+      video::-webkit-media-controls-overlay-play-button {
+        display: none;
+      }
     }
     .image-container {
       display: flex;
