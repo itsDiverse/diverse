6886ed349c5529228a9fff0871598d7c09122079
tdelgio <tomas.dg@me.com>
2020-09-29 16:11:10 -0300

    rm mediaquery #0

--- src/components/widgets/Module0/Module0.js
+++ src/components/widgets/Module0/Module0.js
@@ -3,62 +3,58 @@ import { StyledContentWrapper } from "./Module0.styles"
 
 import { GetFreeProposalButton } from "../../Button"
 import { ModalLink } from "../../ModalLink"
-import { useMediaQuery } from "react-responsive"
 import Brands from "../../../images/Module0/brands.svg"
 
-export default props => {
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1000px)" })
-  return (
-    <StyledContentWrapper theme={props.theme}>
-      <div className="content-wrapper">
-        <div className="container-lg">
-          <div className="toolssection">
-            <div className="tools-section">
-              <div className="video-container">
-                <video
-                  id="Video0"
-                  preload="yes"
-                  autoPlay="autoplay"
-                  playsInline
-                  muted
-                  loop
-                  style={{ outline: "none" }}
-                >
-                  <source
-                    src={props.videos.cubePipesVideo}
-                    type="video/mp4"
-                  ></source>
-                  <source
-                    src={props.videos.cubePipesVideo1}
-                    type="video/m4v"
-                  ></source>
-                  <source
-                    src={props.videos.cubePipesVideo2}
-                    type="video/webm"
-                  ></source>
-                </video>
-              </div>
-              <div className="title-section">
-                <p className="heading-section">{props.data.heading}</p>
-                <h1>{props.data.title}</h1>
-                <p className="subtitle-section">{props.data.text}</p>
-                <div className="powerful-tools-btn">
-                  {props.data.singleButton && (
-                    <ModalLink to="/freeProposal/">
-                      <GetFreeProposalButton to="/freeProposal/">
-                        {props.data.singleButton}
-                      </GetFreeProposalButton>
-                    </ModalLink>
-                  )}
-                </div>
+export default props => (
+  <StyledContentWrapper theme={props.theme}>
+    <div className="content-wrapper">
+      <div className="container-lg">
+        <div className="toolssection">
+          <div className="tools-section">
+            <div className="video-container">
+              <video
+                id="Video0"
+                preload="yes"
+                autoPlay="autoplay"
+                playsInline
+                muted
+                loop
+                style={{ outline: "none" }}
+              >
+                <source
+                  src={props.videos.cubePipesVideo}
+                  type="video/mp4"
+                ></source>
+                <source
+                  src={props.videos.cubePipesVideo1}
+                  type="video/m4v"
+                ></source>
+                <source
+                  src={props.videos.cubePipesVideo2}
+                  type="video/webm"
+                ></source>
+              </video>
+            </div>
+            <div className="title-section">
+              <p className="heading-section">{props.data.heading}</p>
+              <h1>{props.data.title}</h1>
+              <p className="subtitle-section">{props.data.text}</p>
+              <div className="powerful-tools-btn">
+                {props.data.singleButton && (
+                  <ModalLink to="/freeProposal/">
+                    <GetFreeProposalButton to="/freeProposal/">
+                      {props.data.singleButton}
+                    </GetFreeProposalButton>
+                  </ModalLink>
+                )}
               </div>
             </div>
-            <div className="brands-wrapper">
-              <img src={Brands} alt={props.images.brandsAlt} />
-            </div>
+          </div>
+          <div className="brands-wrapper">
+            <img src={Brands} alt={props.images.brandsAlt} />
           </div>
         </div>
       </div>
-    </StyledContentWrapper>
-  )
-}
+    </div>
+  </StyledContentWrapper>
+)
