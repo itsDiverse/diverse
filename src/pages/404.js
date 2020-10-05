dead349ae04f80ffff2df44f36fd72f96a83e3cb
tdelgio <tomas.dg@me.com>
2020-10-05 17:42:20 -0300

    change to lower notfound page

--- src/pages/404.js
+++ src/pages/404.js
@@ -30,7 +30,7 @@ const NotFoundPage = () => (
   <LayoutV1>
     <SEO title="404: Not found" />
     <Wrapper>
-      <h1>NOT FOUND</h1>
+      <h1>Not Found.</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Wrapper>
   </LayoutV1>
