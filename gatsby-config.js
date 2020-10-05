1339885ecf63b2c575ff1f1641f7f99673fe3fe2
tdelgio <tomas.dg@me.com>
2020-10-05 17:58:51 -0300

    revert changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-offline`,
       options: {
-        precachePages: [`/freeProposal/`, `/coomingSoon/`, `/404/`],
+        precachePages: [`/freeProposal/`],
         workboxConfig: {
           runtimeCaching: [
             {
