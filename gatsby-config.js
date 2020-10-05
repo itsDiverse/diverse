e246213f079a947333a977d4b307a8fc976c0b41
tdelgio <tomas.dg@me.com>
2020-10-05 18:09:52 -0300

    add preload pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-offline`,
       options: {
-        precachePages: [`/freeProposal/`],
+        precachePages: [`/freeProposal/`, `/coomingSoon/`, `/404/`],
         workboxConfig: {
           runtimeCaching: [
             {
