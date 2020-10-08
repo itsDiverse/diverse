edd6b84ff4fd65a3708fa5a7cbe789d9cc8b2f23
tdelgio <tomas.dg@me.com>
2020-10-08 20:15:04 -0300

    change path to local repo

--- src/videos.js
+++ src/videos.js
@@ -3,16 +3,16 @@ import sample from "./videos/sample.mp4"
 import sample2 from "./videos/A_Web_Programming_1.mp4"
 import sampleVideo from "./videos/samplevideo.mp4"
 import playIcon from "./images/Module2/play.svg"
-//import cubePipesVideo from "../static/cubePipes.mp4"
-//import cubesVideo from "../static/cubes_2.mp4"
-//import cubesObjects from "../static/cubesObjects.mp4"
-//import comingSoon from "../static/comingSoon.mp4"
+import cubePipesVideo from "../static/cubePipes.mp4"
+import cubesVideo from "../static/cubes_2.mp4"
+import cubesObjects from "../static/cubesObjects.mp4"
+import comingSoon from "../static/comingSoon.mp4"
 //++++++++++++++++++++++++++++++++++++
 
 export const videos = {
   //---------------------------------Module 0
   module0: {
-    cubePipesVideo: "https://www.meetdiverse.com/cubepipes.mp4",
+    cubePipesVideo,
     cubePipesVideo1: "https://www.meetdiverse.com/cubepipes.m4v",
     cubePipesVideo2: "https://www.meetdiverse.com/cubepipes.webm",
   },
@@ -38,18 +38,18 @@ export const videos = {
   },
   //---------------------------------Module 23
   module23: {
-    cubesVideo: "https://www.meetdiverse.com/cubes_2.mp4",
+    cubesVideo,
     cubesVideo1: "https://www.meetdiverse.com/cubes_2.m4v",
     cubesVideo2: "https://www.meetdiverse.com/cubes_2.webm",
   },
   //---------------------------------Module 24
   module24: {
-    cubesObjects: "https://www.meetdiverse.com/cubesobjects.mp4",
+    cubesObjects,
     cubesObjects1: "https://www.meetdiverse.com/cubesobjects.m4v",
     cubesObjects2: "https://www.meetdiverse.com/cubesobjects.webm",
   },
   //---------------------------------ComingSoon Page
   comingSoon: {
-    comingSoon: "https://www.meetdiverse.com/comingsoon.mp4",
+    comingSoon,
   },
 }
