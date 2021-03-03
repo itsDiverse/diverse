64f92f49ec05c1d0b852c08f0ec690df2f4e396b
tdelgio <tomas.dg@me.com>
2021-03-03 12:32:52 -0300

    add ul

--- src/components/privacy.js
+++ src/components/privacy.js
@@ -62,13 +62,17 @@ export const Privacy = () => {
       <Title>Service Providers</Title>
       <Description>
         We may employ third-party companies and individuals due to the following
-        reasons: • To facilitate our Service; • To provide the Service on our
-        behalf; • To perform Service-related services; or • To assist us in
-        analyzing how our Service is used. We want to inform users of this
-        Service that these third parties have access to your Personal
-        Information. The reason is to perform the tasks assigned to them on our
-        behalf. However, they are obligated not to disclose or use the
-        information for any other purpose.
+        reasons:
+        <ul>
+          <li>To facilitate our Service;</li>
+          <li> To provide the Service on our behalf;</li>
+          <li> To perform Service-related services; or </li>
+          <li>To assist us in analyzing how our Service is used.</li>
+        </ul>
+        We want to inform users of this Service that these third parties have
+        access to your Personal Information. The reason is to perform the tasks
+        assigned to them on our behalf. However, they are obligated not to
+        disclose or use the information for any other purpose.
       </Description>
       {""}
       <Title>Security</Title>
